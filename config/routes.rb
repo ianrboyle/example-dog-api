d0034f5d638b138267ee693ccb812db09d1c2319
Ian Boyle <irboyle21@gmail.com>
2021-10-22 14:08:23 -0500

    initial commit. also adds user / sessions stuff - authentification stuff

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  ###USER ROUTES
+  post "/users" => "users#create"
+
+  ###SESSION ROUTES
+  post "/sessions" => "sessions#create"
+
+
+end
